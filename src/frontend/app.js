bc9430d51404cd930d2812673ac1c68271330ebc
Eduardo da Silva <Eduardo.daSilva@studentfr.ch>
2026-02-11 09:31:25 +0100

    couleur bleu violet

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,5 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "https://baqtestappbackend.blackglacier-c7b0844e.northeurope.azurecontainerapps.io/api/tasks";
+const apiEndpoint = "https://backend-210-fi21-dasilva-eduardo.happycliff-3f8669b9.switzerlandnorth.azurecontainerapps.io/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
